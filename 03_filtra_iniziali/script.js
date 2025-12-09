15af50e05a2e85139391f7ac6dcad8349eec4f15
marcotristani <marco.tristani@hotmail.it>
2025-12-09 15:06:35 +0100

    flat: esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const initialStringArray = ( myarray, initial) => {
+    let stringArray = [];
+    let contatour = 0;
+    for (let i = 0; i < myarray.length; i++) {
+        if ( initial === myarray[i][0]){
+            stringArray[contatour] = myarray[i];
+            contatour++;
+        } 
+    }
+    return stringArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(initialStringArray(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
