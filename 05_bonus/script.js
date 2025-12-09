c2bbe3ae74eb0cbae2186a57fc32d7459f48855c
marcotristani <marco.tristani@hotmail.it>
2025-12-09 15:57:56 +0100

    flat: esercizio 5

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,12 +7,21 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
-
+const greetingTime = myname => {
+    const currentTime = new Date();
+    const currentHour =currentTime.getHours();
+    let greeting = 'buonasera';
+    if( currentHour <= 13){
+        greeting = 'buongiorno';
+    }else if ( currentHour <= 17){
+        greeting = 'buon pomeriggio';
+    }
+    return `${greeting} ${myname}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetingTime(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
