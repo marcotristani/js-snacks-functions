c90f0d380af2d8110159f69743c22738f8c02311
marcotristani <marco.tristani@hotmail.it>
2025-12-09 15:33:45 +0100

    flat: esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,26 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const counterVowels = mystring => {7
+    let counterVolwes = 0;
+    let mystringVolwes = [];
+    for (let i = 0; i < mystring.length; i++) {                     //ciclo per prendere una ad una le lettere della mia string
+        const vowels = ['a','e','i','o','u'];                       //stringa di vocali
+        for (let index = 0; index < vowels.length; index++) {       //ciclo per confrontare la mia iesima lettera della stringa con ogni vocale singolarmente
+            if( vowels[index] === mystring[i].toLowerCase()){
+                mystringVolwes[counterVolwes] = mystring[i];
+                counterVolwes++;
+            }   
+        } 
+    }
+    return mystringVolwes;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const volwesWord = counterVowels(word);
+const counterVolwesWord = volwesWord.length;
+console.log(counterVolwesWord, volwesWord);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
