3a5df794d8cd6a02be68b4320d5963756ddcdfa3
marcotristani <marco.tristani@hotmail.it>
2025-12-09 15:15:04 +0100

    flat: generalizzato esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,23 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const initial = 'a';
 
 // Dichiara la funzione qui.
 const initialStringArray = ( myarray, initial) => {
     let stringArray = [];
-    let contatour = 0;
+    let counter = 0;
     for (let i = 0; i < myarray.length; i++) {
-        if ( initial === myarray[i][0]){
-            stringArray[contatour] = myarray[i];
-            contatour++;
+        if ( initial.toLowerCase() === myarray[i][0].toLowerCase()){
+            stringArray[counter] = myarray[i];
+            counter++;
         } 
     }
     return stringArray;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(initialStringArray(names, 'A'));
+console.log(initialStringArray(names, initial));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
